8e604fe2de6043f7be516d972ef5fc5481bb7e4a
NoahSec <antonygustavo10202016@gmail.com>
2023-12-06 23:21:02 +0000

    API/Crud

---
+++ backend/API/User/Delete.ts
@@ -0,0 +1,21 @@
+import { UserSchema } from "../../schema/userSchema";
+import { Request, Response} from 'express';
+
+export async function UserDelete(req:Request, res:Response) {
+    const {id} = req.params
+
+    try{
+        const user = await UserSchema.findById(id)
+        if(!user){
+            return res.status(404).json({msg:'User not found!'})
+        }
+
+        await user.deleteOne({_id:id})
+        res.status(200).json({msg:'User delete successfully!'})
+
+    }catch(err){
+        console.error(err)
+        res.status(500).json({msg:'Error delete user!'})
+    }
+    
+}
---
+++ backend/API/User/showItens.ts
@@ -0,0 +1,15 @@
+import { Request,Response } from 'express'
+import { UserSchema } from "../../schema/userSchema";
+
+export async function ShowItens(req:Request, res:Response) {
+    try{
+        const itens = await UserSchema.find()
+        res.status(200).json({itens})
+
+
+    }catch(err){
+
+        res.status(404).json({msg:'Itens not found!'})
+
+    }
+}
