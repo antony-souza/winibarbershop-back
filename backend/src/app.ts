f2e833fa15a70afc3a501ca197c024d6cb7731de
NoahSec <antonygustavo10202016@gmail.com>
2023-12-08 11:04:48 +0000

    Update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,23 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import { DataBase } from '../db/mongooseDB'
-import { port, portMsg } from '../variables/variablesGlobal'
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import { router } from '../router/router';
+import { DataBase } from '../db/mongooseDB';
+import { port, portMsg } from '../variables/variablesGlobal';
+import { UserDelete } from '../API/User/Delete';
+import { ShowItens } from '../API/User/showItens';
+import { CheckUser, CreateUser, ValidationUser } from '../API/User/Create';
 dotenv.config()
 DataBase()
 
 const app = express()
+app.use(express.json());
+app.use(cors())
+
+router.get('/showitens', ShowItens)
+router.post('/createUser', ValidationUser,CheckUser,CreateUser)
+router.put('/update/:id', )
+router.delete('/delete/:id',UserDelete)
 
 app.listen(port,()=>{
     console.log(portMsg)
