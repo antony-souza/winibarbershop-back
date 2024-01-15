20d21e8a236029d1a2a67bc9e250798ec155fa59
antony-souza <antonygustavo09@hotmail.com>
2024-01-14 21:28:51 -0300

    pc novo

---
+++ variables/variablesGlobal.ts
@@ -0,0 +1,3 @@
+export const mongoDB = `mongodb+srv://antonysouza:bMfBwmFBag7U1hIL@cluster0.xq19opq.mongodb.net/`;
+export const port = process.env.PORT || 8100;
+export const portMsg = 'Servidor Conectado!'
