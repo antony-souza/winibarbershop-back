36523ecd91d404e4cadaefba51eab0a6b011a607
antony-souza <antonygustavo09@hotmail.com>
2024-01-18 21:43:58 -0300

    up

--- variables/variablesGlobal.ts
+++
@@ -1,3 +0,0 @@
-export const mongoDB = `mongodb+srv://antonysouza:bMfBwmFBag7U1hIL@cluster0.xq19opq.mongodb.net/`;
-export const port = process.env.PORT || 8100;
-export const portMsg = 'Servidor Conectado!'
